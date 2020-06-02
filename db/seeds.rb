c793e41a53f84efb99c0509b0f7694ebb6c8f541
Martin Schmalz <msacc10001@gmail.com>
2020-06-02 14:47:09 +0200

    club-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database Clubs ...'
+Club.destroy_all
+puts 'Cleaning database Players ...'
+Player.destroy_all
+puts 'Cleaning database Users ...'
+User.destroy_all
+puts ''
+puts 'Creating User ...'
+
+User.create(email: "bob@gmail.com", password: "123456")
+
+clubs = [{ name: 'VfR Aalen',
+           user_id: User.first.id },
+         { name: 'SC Charlottenburg',
+           user_id: User.first.id },
+         { name: 'FSV Salmrohr',
+           user_id: User.first.id },
+         { name: 'Wormatia Worms',
+           user_id: User.first.id }]
+clubs.each do |attributes|
+  club = Club.create!(attributes)
+  puts "Created #{club.name}"
+end
+puts 'Finished!'
