513231b16be42ca770921109f7d47a16737b4a6f
Martin Schmalz <msacc10001@gmail.com>
2020-06-02 17:11:22 +0200

    fix-seeds

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,11 +3,10 @@ class Player < ApplicationRecord
   belongs_to :club, optional: true
   has_many :statistics
   has_many :tournaments, through: :statistics
-
-  SKILLS = [ "speed", "strong", "endurance", "passing", "shooting", "technique", "teamplayer", "tactic"]
-
+  # geocode
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-
-  validates :skills, inclusion: { in: SKILLS }
+  # skills
+  SKILL = ["speed", "strong", "endurance", "passing", "shooting", "technique", "teamplayer", "tactic"]
+  validates :skill, inclusion: { in: SKILL }
 end
