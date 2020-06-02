9eef2d35da2dd448f364bb779436fa89ecbcbb48
Josue Sirker <jsirkerj@gmail.com>
2020-06-02 13:16:33 +0200

    added statistics seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Statistic.destroy_all
+Player.destroy_all
+User.destroy_all
+user = User.create!(email: "bob@gmail.com", password: "123456")
+Player.create!(name: "George", user_id: User.first.id)
+Tournament.create!(state: "Bayern", season_year: 2020)
+Player.first.statistics.create!(
+  [
+      {game_qty: 400, goal_qty: 0, starting_eleven_avg: 5, tournament_id: Tournament.first.id },
+      {game_qty: 20, goal_qty: 2, starting_eleven_avg: 30, tournament_id: Tournament.first.id },
+      {game_qty: 10, goal_qty: 10 , starting_eleven_avg: 100, tournament_id: Tournament.first.id },
+      {game_qty: 600, goal_qty: 50 , starting_eleven_avg: 80, tournament_id: Tournament.first.id }
+  ]
+)
