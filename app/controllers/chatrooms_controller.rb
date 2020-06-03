6ee4698c4bcbbe2f813e2dfb6700b4cb2608bfb8
Martin Schmalz <msacc10001@gmail.com>
2020-06-03 18:03:53 +0200

    chatroom-index-links-only

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,9 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = policy_scope(Chatroom.all)
+    # authorize(@chatrooms)
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
