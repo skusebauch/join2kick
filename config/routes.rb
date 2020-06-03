8f37d17a831ea610921990b5a16f712d7ac14669
Julius Passow <juliuspassow@web.de>
2020-06-03 12:16:51 +0200

    players home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'clubs/show'
   devise_for :users
   root to: 'pages#home'
- 
+
   resources :clubs, only: [:show]
   resources :players, only: [:index, :show]
 
