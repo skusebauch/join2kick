0fc70c806f8fedca34497b875f7a15dc212f1041
Martin Schmalz <msacc10001@gmail.com>
2020-06-05 12:32:05 +0200

    chatrooms-index - Users can only see messages to another User

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,9 +1,12 @@
 class ChatroomsController < ApplicationController
   def index
     @chatrooms = policy_scope(Chatroom.all)
+    # Message.where(" user_id = ? ", current_user.id).distinct(:chatroom_id)
     # @my_chatrooms = Message.where(user_id: current_user.id)
     # raise
     # authorize(@chatrooms)
+    # raise
+    @chatroom_already_displayed = false
   end
 
   def show
