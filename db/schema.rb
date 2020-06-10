a730aebe030cb107ceb858911744d55368a1bbac
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-06-10 17:07:06 +0200

    change to name at messages

--- db/schema.rb
+++ db/schema.rb
@@ -142,7 +142,6 @@ ActiveRecord::Schema.define(version: 2020_06_10_123923) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
     t.string "name"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
