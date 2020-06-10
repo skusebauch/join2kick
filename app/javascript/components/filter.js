88e436d47847dd5db6908a14c03a2569481742b4
Josue Sirker <jsirkerj@gmail.com>
2020-06-10 10:33:00 +0200

    fixed wish list

--- app/javascript/components/filter.js
+++ app/javascript/components/filter.js
@@ -1,11 +1,12 @@
 const initFilter = () => {
-  const starClass = document.querySelectorAll(".fa fa-star");
-  if (starClass != undefined) {
-    starClass.forEach(filter => {
+ const playerClass = document.querySelectorAll(".filter-player");
+  if (playerClass != undefined) {
+    playerClass.forEach(filter => {
       filter.addEventListener("click", (event) => {
-      event.currentTarget.classList.toggle("active-star");
-      });
+      event.currentTarget.classList.toggle("active");
     });
+  });
+
   }
 }
 
