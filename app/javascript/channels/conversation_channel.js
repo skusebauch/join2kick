2707d9ece59e34a7ba150ebeebe59a82576b95d2
Martin Schmalz <msacc10001@gmail.com>
2020-06-11 10:28:50 +0200

    no scrolling down when new message

--- app/javascript/channels/conversation_channel.js
+++ app/javascript/channels/conversation_channel.js
@@ -4,10 +4,8 @@ const initConversationCable = () => {
   const messagesContainer = document.getElementById('messages-container');
   if (messagesContainer) {
     const id = messagesContainer.dataset.conversationId;
-    //console.log(id)
     consumer.subscriptions.create({ channel: "ConversationChannel", id: id }, {
       received(data) {
-        // called when data is broadcast in the cable
         messagesContainer.insertAdjacentHTML('beforeend', data);
       },
     });
