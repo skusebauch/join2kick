b57ed9d857830a5ff988293484738ff3521031ce
Julius Passow <juliuspassow@web.de>
2020-06-11 15:51:06 +0200

    added liking function and post upload and value proposition

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :avatar)
   end
 end
